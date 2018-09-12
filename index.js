af01c081f4db797a3089ca12ae83788b77e32c0b
somsasha <somsasha111@gmail.com>
2018-09-12 22:55:25 +0300

    print all command line arguments

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-const name = process.argv[2];
+const name = [];
 
-console.log(`Hi ${name}!`);
+for (var i = 2; i<process.argv.length; i++) name[i-2] = process.argv[i];
+
+function con(mas) {
+    var s = ""
+    for(var i=0; i<mas.length; i++) s += mas[i] + " "
+    return s
+  }
+
+console.log(`Hi ${con(name)}!`);
+
+
+    
